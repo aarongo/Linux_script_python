7b6543351fa9482ecb3d1ee55785b0f4b9e0f494
aarongo <aaron.docker@gmail.com>
2015-08-24 18:27:11 +0800

    add file delete three days code

--- mongodb.py
+++ mongodb.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # _*_coding:utf-8_*_
 '''Author       lonnyliu
     Use Of      Linux backup file
@@ -33,10 +32,13 @@ tar_dir = "%s%s-%s.tar.gz" % (back_dir_tmp, now, mongodb_name)
 command_mv = "cd %s && mv %s-%s.tar.gz %s" % (back_dir_tmp, now, mongodb_name, back_dir)
 # 删除临时备份目录
 command_rm = "rm -rf %s" % (back_dir_tmp)
+# 删除几天前的备份
+day = 1
+command_delete = "find %s -mtime +%s -delete" % (back_dir, day)
 
 
 class Mongodb(object):
-    def __init__(self, command_mk, command_dump, command_tar, command_mv, command_rm):
+    def __init__(self, command_mk, command_dump, command_tar, command_mv, command_rm, command_delete):
         self.command_mk = command_mk
         self.command_dump = command_dump
         self.command_tar = command_tar
@@ -56,7 +58,7 @@ class Mongodb(object):
                 com_tar = subprocess.Popen(command_tar, shell=True)
                 com_tar.wait()
                 tar_file_size = os.path.getsize(tar_dir) / 1024 / 1024
-                print "Mongodb的备份文件大小为：%sM"%(tar_file_size)
+                print "Mongodb的备份文件大小为：%sM" % (tar_file_size)
                 if com_tar.returncode == 0:
                     print "--------\033[32m执行最终备份 \033[0m-------"
                     com_mv = subprocess.Popen(command_mv, shell=True)
@@ -78,6 +80,18 @@ class Mongodb(object):
         else:
             print "--------\033[31m 执行mongodb备份到临时文件目录失败 \033[0m-------"
 
+    def drop(self):
+        #先获取文件个数
+        for dir_path, subpaths, files in os.walk(back_dir):
+            if len(files) > day:
+                print "----------------\033[32m删除%s天前备份\033[0m--------" % day
+                com_delete = subprocess.Popen(command_delete, shell=True)
+                if com_delete.returncode == 0:
+                    print "------- \033[32m 删除成功\033[0m-------"
+                else:
+                    print "--------\033[31m 删除失败\033[0m --------"
 
-run = Mongodb(command_mk, command_dump, command_tar, command_mv, command_rm)
+
+run = Mongodb(command_mk, command_dump, command_tar, command_mv, command_rm, command_delete)
 run.backup()
+run.drop()
