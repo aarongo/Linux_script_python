c4bc8b9dcd8a8779ff1bc1847b7c47ed8d2ece43
aarongo <aaron@126.com>
2015-08-25 22:52:02 +0800

    Optimizing Content

--- tomcat.py
+++ tomcat.py
@@ -10,20 +10,25 @@ import subprocess
 import time, datetime
 import os
 
-action_start = ['start', 'stop', 'restart', 'info', 'detect']
 home = "/install/tomcat/"
-tomcat_log = "%slogs" % (home)
 script = "startup.sh"
 command_start = "%sbin/%s" % (home, script)
-tomcat_pid = """ ps aux | grep tomcat | grep -v "grep" | awk '{print $2}' > /root/pid.txt"""
-now = datetime.datetime.now().strftime("%Y-%m-%d-%H")
 
 
 class MyTomcat(object):
-    def __init__(self, home, script, command_start, ):
+    def __init__(self, home, script, command_start):
         self.home = home
         self.script = script
         self.command_start = command_start
+
+    def t_pid(self):
+        tomcat_pid = """ ps aux | grep tomcat | grep -v "grep" | awk '{print $2}' > /root/pid.txt"""
+        get_pid = subprocess.Popen(tomcat_pid, shell=True)
+        get_pid.wait()
+        pid = file("/root/pid.txt", "r")
+        pid_num = int(pid.readline().strip())
+        pid.close()
+        return pid_num
 
     def start(self):
         print "Running start"
@@ -36,43 +41,31 @@ class MyTomcat(object):
 
     def stop(self):
         print "Running stop"
-        get_pid = subprocess.Popen(tomcat_pid, shell=True)
-        get_pid.wait()
-        pid = file("/root/pid.txt", "r")
-        pid_num = pid.readline().strip()
-        stop = "kill -9 %s" % pid_num
+        stop = "kill -9 %s" % self.t_pid()
         com_stop = subprocess.Popen(stop, shell=True)
         com_stop.wait()
-        pid.close()
         if com_stop.returncode == 0:
             print "--------Stoping Tomcat Sucessful--------"
         else:
             print "--------Stoping Tomcat Fail--------"
 
     def info(self):
-        print "Running info"
+        tomcat_log = "%slogs" % (home)
+        now = datetime.datetime.now().strftime("%Y-%m-%d-%H")
         print "--------Tomcat Running Info--------"
-        pid = file("/root/pid.txt", "r")
-        pid_num = pid.readline().strip()
-        pid.close()
         for dir_path, subpaths, files in os.walk(tomcat_log):
             print '''
                     TOMCAT_PID:      %s
                     TOMCAT_HOME:     %s
                     DATE_TIME:       %s
                     TOMCAT_LOG_NUM : %s
-            ''' % (pid_num, home, now, len(files))
+            ''' % (self.t_pid(), home, now, len(files))
 
     def restart(self):
         print "Running stop"
-        get_pid = subprocess.Popen(tomcat_pid, shell=True)
-        get_pid.wait()
-        pid = file("/root/pid.txt", "r")
-        pid_num = pid.readline().strip()
-        stop = "kill -9 %s" % pid_num
+        stop = "kill -9 %s" % self.t_pid()
         com_stop = subprocess.Popen(stop, shell=True)
         com_stop.wait()
-        pid.close()
         if com_stop.returncode == 0:
             print "--------Stoping Tomcat Sucessful--------"
         else:
@@ -88,6 +81,7 @@ class MyTomcat(object):
 
 
 if __name__ == "__main__":
+    action_start = ['start', 'stop', 'restart', 'info', 'detect']
     while True:
         run = MyTomcat(home, script, command_start)
         print "\033[32m ------You need to enter one of the following!------\033[0m"
