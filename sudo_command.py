65b868c80f79ba3e6ec824b3982edefa336654cc
aarongo <lonny@126.com>
2015-09-07 18:22:21 +0800

    实现Linux 自动输入密码执行命令与localhost所使用的脚本

--- sudo_command.py
+++ sudo_command.py
@@ -1,10 +1,11 @@
 # _*_coding:utf-8_*_
 __author__ = 'lonnyliu'
-import pexpect
+import pexpect, getpass
 
 
 class AutoPassword(object):
-    def __init__(self, command, passd):
+    def __init__(self, sys_user, command, passd):
+        self.sys_user = sys_user
         self.command = command
         self.passd = passd
 
@@ -13,23 +14,20 @@ class AutoPassword(object):
         # --------执行Linux 系统命令--------
         child = pexpect.spawn(self.command)
         # --------探测屏幕输出信息--------
-        child.expect("password for yulong:")
+        child.expect(self.sys_user)
         # "-------发送密码-------"
         child.sendline(self.passd)
         # "-------收集结果-------"
         child.expect(pexpect.EOF)
-        print "-------------------------\033[32m结果为 \033[0m-------------------------",child.before
-
+        print "-------------------------\033[32m结果为 \033[0m-------------------------", child.before
 
 if __name__ == "__main__":
     while True:
+        sys_user_info = "password for" + " " + getpass.getuser()
         password = "aarongo"
-        # 执行单个命令时可以写死
-        # com = "sudo /home/yulong/.pyenv/versions/2.7.10/bin/python2.7 /home/yulong/.pyenv/versions/2.7.10/bin/sslocal -c /etc/shadowsocks.json -d start"
-
         # 执行多条命令 与while Ture 结合
         com = "sudo" + " " + str(raw_input("Please Input Commands:"))
-        run = AutoPassword(com, password)
+        run = AutoPassword(sys_user_info, com, password)
         run.start()
         if com.endswith("quit"):
             print "--------\033[31m退出该SHELL \033[0m--------"
