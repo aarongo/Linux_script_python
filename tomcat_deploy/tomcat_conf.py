b7466390140376f052f9a98b01049e6aab1c11e3
aarongo <lonny@126.com>
2015-09-22 17:32:52 +0800

    New changelist

---
+++ tomcat_deploy/tomcat_conf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# _*_coding:utf-8_*_
+
+
+"""
+15-9-22
+author : 'yulong'
+Good memory than rotten written
+"""
+# bulid 选项
+svn_bin_home = "/usr/bin/svn"
+svn_checkout_dir = "/install/cybershop-B2B2C-test/"
+maven_bin_home = "/install/maven/bin/mvn"
+# 远程主机
+remote_host = ['172.31.1.101', '172.31.1.100']
