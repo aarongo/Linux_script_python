f20fb770e90b187e1a828f1ec248a33123e74262
LonnyLiu <lonnyliu@126.com>
2015-09-30 16:45:52 +0800

    New tomcat auto deploy

--- tomcat_deploy/tomcat_conf.py
+++ tomcat_deploy/tomcat_conf.py
@@ -12,13 +12,17 @@ svn_bin_home = "/usr/bin/svn"
 svn_checkout_dir = "/install/cybershop-B2B2C-test/"
 maven_bin_home = "/install/maven/bin/mvn"
 # 临时解压 war 包文件夹
-unzip_tmp = "/install/unzip_tmp"
-unzip_war_front = "/install/unzip_tmp/cybershop-front-0.0.1-SNAPSHOT"
-unzip_war_web = "/install/unzip_tmp/cybershop-web-0.0.1-SNAPSHOT"
+project_dir = "/install/unzip_project"
+unzip_war_front = "%s/cybershop-front-0.0.1-SNAPSHOT" % project_dir
+unzip_war_web = "%s/cybershop-web-0.0.1-SNAPSHOT" % project_dir
 war_path = ["/install/cybershop-B2B2C-test/cybershop-front/target/",
             "/install/cybershop-B2B2C-test/cybershop-web/target/"]
-# 远程主机
+# 远程主机 将此目录里的项目目录连接到 tomcat 部署目录中
 remote_host_dir = {
-    '172.31.1.100': '/root/war_test/',
-    '172.31.1.101': '/root/war_test/'
+    # '172.31.1.100': '/install/cybershop_project/',
+    # '172.31.1.101': '/install/cybershop_project/'
+    '10.90.10.169': '/install/cybershop_project/'
 }
+remote_host_user = 'root'
+remote_host_password = 'comall2014'
+
