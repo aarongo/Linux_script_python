03d5ed9a3d04e3c2d04baa8fd20bac8f3e66ed6a
LonnyLiu <lonnyliu@126.com>
2015-10-14 00:50:06 +0800

    Mysql Memcached Mongodb 自动安装

--- mysql/auto_config.py
+++ mysql/auto_config.py
@@ -1,13 +1,14 @@
 # _*_coding:utf-8_*_
+#######################Mysql###################
 # 软件存放路径
 software_dir = "/software/packages"
 # mysql verson
 version = ['mysql-5.1.73', 'mysql-5.5.45', 'mysql-5.6.26']
 # mysql 现在 URL
 
-mysql51_d_url = "http://192.168.1.234/serverpackages/Mysql-5.5/mysql-5.1.73.tar.gz"
-mysql55_d_url = "http://192.168.1.234/serverpackages/Mysql-5.5/mysql-5.5.45.tar.gz"
-mysql56_d_url = "http://192.168.1.234/serverpackages/Mysql-5.5/mysql-5.6.26.tar.gz"
+mysql51_d_url = "http://172.31.1.160/serverpackages/Mysql-5.5/mysql-5.1.73.tar.gz"
+mysql55_d_url = "http://172.31.1.160/serverpackages/Mysql-5.5/mysql-5.5.45.tar.gz"
+mysql56_d_url = "http://172.31.1.160/serverpackages/Mysql-5.5/mysql-5.6.26.tar.gz"
 # 准备工作
 cmake33_d_url = "http://192.168.1.234/serverpackages/Mysql-5.5/cmake-3.3.2.tar.gz"
 cmake_bin_path = "/usr/local/bin/cmake"
@@ -15,7 +16,7 @@ cmake_name = "cmake-3.3.2"
 default_install_software = "wget tar ncurses  ncurses-devel  openssl openssl-devel zlib bison"
 # 编译选项
 install_directory = "-DCMAKE_INSTALL_PREFIX=/software/mysql"
-data_directory = "-DMYSQL_DATADIR=/software/data"
+data_directory = "-DMYSQL_DATADIR=/software/data/mysql"
 character_set = "-DDEFAULT_CHARSET=utf8"
 # 默认字符校对
 default_encoding = "-DDEFAULT_COLLATION=utf8_unicode_ci"
@@ -49,3 +50,78 @@ mysql55 = mysql55_d_url.split("//")[1].split('/')[3].split('.tar.gz')[0]
 mysql56 = mysql56_d_url.split("//")[1].split('/')[3].split('.tar.gz')[0]
 mysql_bin = install_directory.split('=')[1]
 mysql_data = data_directory.split('=')[1]
+#######################Msyql End########################################
+
+###############################memcached################################
+# memcached url
+memcached_url = "http://172.31.1.160/serverpackages/Memcacehd/memcached-1.4.24.tar.gz"
+# libevent zlib库 url
+libevent_url = "http://172.31.1.160/serverpackages/Memcacehd/libevent-2.0.22-stable.tar.gz"
+zlib_url = "http://172.31.1.160/serverpackages/Memcacehd/zlib-1.2.8.tar.gz"
+
+# 获取软件包名
+libevent_pack_name = libevent_url.split('//')[1].split('/')[3]
+zlib_pack_name = zlib_url.split('//')[1].split('/')[3]
+memcached_pack_name = memcached_url.split('//')[1].split('/')[3]
+# 软件名称
+libevent_name = libevent_url.split('//')[1].split('/')[3].split('.tar.gz')[0]
+zlib_name = zlib_url.split('//')[1].split('/')[3].split('.tar.gz')[0]
+memcached_name = memcached_url.split('//')[1].split('/')[3].split('.tar.gz')[0]
+# 编译安装选项-- libevent
+libevent_parameters = "--prefix=/usr/local/libevent"
+libevent_dir = "/usr/local/libevent"
+# 编译安装选项--zlib
+zlib_paramenters = "--prefix=/usr/local/zlib"
+zlib_dir = "/usr/local/zlib"
+# 编译安装memcached 选项
+memcached_install_dir = "/software/memcached"
+memcached_parameters = "--prefix=%s --with-libevent=/usr/local/libevent" % memcached_install_dir
+memcached_prot = [11211, 11212, 11213, 11214]
+###################################Memcached End################################
+
+###################################Mongodb######################################
+# mongodb url
+mongodb_url = "http://172.31.1.160/serverpackages/Mongodb/mongodb-linux-x86_64-rhel62-3.0.6.gz"
+# 软件包名称
+mongodb_pack_name = mongodb_url.split('//')[1].split('/')[3]
+mongodb_name = mongodb_url.split('//')[1].split('/')[3].split('.gz')[0]
+# 安装路径
+mongodb_default_dir = "/software/mongodb3"
+mongodb_bin_home = "%s/bin" % mongodb_default_dir
+mongodb_data_dir = "/software/data/mongodb"
+mongodb_config = """
+systemLog:
+ destination: file
+###日志存储位置
+ path: /software/data/mongod.log
+ logAppend: true
+storage:
+##journal配置
+ journal:
+  enabled: true
+##数据文件存储位置
+ dbPath: /software/data/mongodb
+##是否一个库一个文件夹
+ directoryPerDB: true
+##数据引擎
+ engine: wiredTiger
+##WT引擎配置
+ wiredTiger:
+  engineConfig:
+##WT最大使用cache（根据服务器实际情况调节）
+   cacheSizeGB: 10
+##是否将索引也按数据库名单独存储
+   directoryForIndexes: true
+##表压缩配置
+  collectionConfig:
+   blockCompressor: zlib
+##索引配置
+  indexConfig:
+   prefixCompression: true
+#后台运行
+processManagement:
+ fork: true
+##端口配置 默认监听所有
+net:
+ port: 27017
+"""
--- mysql/auto_install_Mysql.py
+++ mysql/auto_install_Mysql.py
@@ -11,94 +11,232 @@ import time
 
 
 class Install_Mysql(object):
-    def software(self):
+    def software_mysql(self):
+        global ret
+        ret = -1
         # 安装软件
         print "\033[32m==========Install Software==========\033[0m"
         command_line1 = "yum -y install %s " % (auto_config.default_install_software)
         subprocess.call(command_line1, shell=True)
-        print "\033[32m==========Compile and install Cmake==========\033[0m"
-        command_line2 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && ./configure && make && make install" % (
-            auto_config.software_dir, auto_config.cmake33_d_url, auto_config.cmake_name, auto_config.cmake_name)
-        subprocess.call(command_line2, shell=True)
-        print "\033[32m===========Install Mysql==========\033[0m"
-        for index, version in enumerate(auto_config.version):
-            print index, version
-        chose_mysql_version = raw_input("\033[32mChost Mysql Version:\033[0m")
-        if chose_mysql_version.isdigit():
-            chose_mysql_version = int(chose_mysql_version)
-            # 定义全局属性,方便后续复制文件使用
-            global new_mysql_dir
-            new_mysql_dir = auto_config.version[chose_mysql_version]
-            if auto_config.version[chose_mysql_version] == auto_config.mysql51:
-                command_line3 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && %s" % (
-                    auto_config.software_dir, auto_config.mysql51_d_url, auto_config.version[chose_mysql_version],
-                    auto_config.version[chose_mysql_version], auto_config.compile_command)
-                subprocess.call(command_line3, shell=True)
-            elif auto_config.version[chose_mysql_version] == auto_config.mysql55:
-                command_line4 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && %s" % (
-                    auto_config.software_dir, auto_config.mysql55_d_url, auto_config.version[chose_mysql_version],
-                    auto_config.version[chose_mysql_version], auto_config.compile_command)
-                subprocess.call(command_line4, shell=True)
-            elif auto_config.version[chose_mysql_version] == auto_config.mysql56:
-                command_line5 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && %s" % (
-                    auto_config.software_dir, auto_config.mysql56_d_url, auto_config.version[chose_mysql_version],
-                    auto_config.version[chose_mysql_version], auto_config.compile_command)
-                subprocess.call(command_line5, shell=True)
+        if os.path.exists(auto_config.cmake_bin_path):
+            print "\033[32m###########Cmake Is Installed##########\033[0m"
+        else:
+            print "\033[32m==========Compile and install Cmake==========\033[0m"
+            command_line2 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && ./configure && make && make install" % (
+                auto_config.software_dir, auto_config.cmake33_d_url, auto_config.cmake_name, auto_config.cmake_name)
+            subprocess.call(command_line2, shell=True)
+        if os.path.exists(auto_config.mysql_bin):
+            print "\033[32m##########Mysql Is Installed##########\033[0m"
+            ret = 0
+        else:
+            print "\033[32m===========Install Mysql==========\033[0m"
+            for index, version in enumerate(auto_config.version):
+                print index, version
+            chose_mysql_version = raw_input("\033[32mChost Mysql Version:\033[0m")
+            if chose_mysql_version.isdigit():
+                chose_mysql_version = int(chose_mysql_version)
+                # 定义全局属性,方便后续复制文件使用
+                global new_mysql_dir
+                new_mysql_dir = auto_config.version[chose_mysql_version]
+                if auto_config.version[chose_mysql_version] == auto_config.mysql51:
+                    command_line3 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && %s" % (
+                        auto_config.software_dir, auto_config.mysql51_d_url, auto_config.version[chose_mysql_version],
+                        auto_config.version[chose_mysql_version], auto_config.compile_command)
+                    subprocess.call(command_line3, shell=True)
+                elif auto_config.version[chose_mysql_version] == auto_config.mysql55:
+                    command_line4 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && %s" % (
+                        auto_config.software_dir, auto_config.mysql55_d_url, auto_config.version[chose_mysql_version],
+                        auto_config.version[chose_mysql_version], auto_config.compile_command)
+                    subprocess.call(command_line4, shell=True)
+                elif auto_config.version[chose_mysql_version] == auto_config.mysql56:
+                    command_line5 = "cd %s && wget %s && tar xzf %s.tar.gz && cd %s && %s" % (
+                        auto_config.software_dir, auto_config.mysql56_d_url, auto_config.version[chose_mysql_version],
+                        auto_config.version[chose_mysql_version], auto_config.compile_command)
+                    subprocess.call(command_line5, shell=True)
 
-    def initialization(self):
-        # 初始化数据库
-        # create mysql user
-        command_line1 = "useradd -s /sbin/nologin mysql && chown -R %s" % (auto_config.mysql_data)
-        subprocess.call(command_line1, shell=True)
-        print command_line1
-        command_line2 = "cd %s/scripts && ./mysql_install_db --user=mysql --basedir=%s --datadir=%s" % (
-            auto_config.mysql_bin, auto_config.mysql_bin, auto_config.mysql_data)
-        subprocess.call(command_line2, shell=True)
-        print command_line2
-        # 复制配置文件
-        command_line3 = "cp -f %s/%s/support-files/my-medium.cnf /etc/my.cnf" % (
-            auto_config.software_dir, new_mysql_dir)
-        subprocess.call(command_line3, shell=True)
-        # 添加到系统命令
-        command_line4 = "cd %s && cp support-files/mysql.server /etc/init.d/mysqld" % auto_config.mysql_bin
-        subprocess.call(command_line4, shell=True)
-        # 如果不启动 更改不了密码
-        command_line5 = "/etc/init.d/mysqld start"
-        subprocess.call(command_line5, shell=True)
-        # 更改默认 root 密码
-        pwd = raw_input("\033[32mPlease Input Root Password:\033[0m")
-        all_host = "%"
-        select_root_password = "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%s' IDENTIFIED BY '%s' WITH GRANT OPTION;" % (
-            all_host, pwd)
-        print select_root_password
-        select_root_password_local = "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '%s' WITH GRANT OPTION;" % pwd
-        print select_root_password_local
-        command_line6 = """%s/bin/mysql -uroot  -e "%s" """ % (auto_config.mysql_bin, select_root_password)
-        command_line7 = """%s/bin/mysql -uroot  -e "%s" """ % (
-            auto_config.software_dir, select_root_password_local)
-        subprocess.call(command_line6, shell=True)
-        subprocess.call(command_line7, shell=True)
+    def initialization_mysql(self):
+        if ret == 0:
+            print "\033[32m##########This Mysql Is Configed##########\033[0m"
+        else:
+            # 初始化数据库
+            # create mysql user
+            command_line1 = "useradd -s /sbin/nologin mysql && chown -R %s" % (auto_config.mysql_data)
+            subprocess.call(command_line1, shell=True)
+            print command_line1
+            command_line2 = "cd %s/scripts && ./mysql_install_db --user=mysql --basedir=%s --datadir=%s" % (
+                auto_config.mysql_bin, auto_config.mysql_bin, auto_config.mysql_data)
+            subprocess.call(command_line2, shell=True)
+            print command_line2
+            # 复制配置文件
+            command_line3 = "cp -f %s/%s/support-files/my-medium.cnf /etc/my.cnf" % (
+                auto_config.software_dir, new_mysql_dir)
+            subprocess.call(command_line3, shell=True)
+            # 添加到系统命令
+            command_line4 = "cd %s && cp support-files/mysql.server /etc/init.d/mysqld" % auto_config.mysql_bin
+            subprocess.call(command_line4, shell=True)
+            # 如果不启动 更改不了密码
+            command_line5 = "/etc/init.d/mysqld start"
+            subprocess.call(command_line5, shell=True)
+            # 更改默认 root 密码
+            pwd = raw_input("\033[32mPlease Input Root Password:\033[0m")
+            all_host = "%"
+            select_root_password = "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%s' IDENTIFIED BY '%s' WITH GRANT OPTION;" % (
+                all_host, pwd)
+            print select_root_password
+            select_root_password_local = "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '%s' WITH GRANT OPTION;" % pwd
+            print select_root_password_local
+            command_line6 = """%s/bin/mysql -uroot  -e "%s" """ % (auto_config.mysql_bin, select_root_password)
+            command_line7 = """%s/bin/mysql -uroot  -e "%s" """ % (
+                auto_config.software_dir, select_root_password_local)
+            subprocess.call(command_line6, shell=True)
+            subprocess.call(command_line7, shell=True)
 
-    def first_reday(self):
+    def reday_mysql(self):
         if os.path.exists(auto_config.software_dir):
             print "\033[32m##########Directory Exist ##########\033[0m"
-            self.software()
-            self.initialization()
+            self.software_mysql()
+            self.initialization_mysql()
         else:
             print "\033[31m##########Directory No Exist ##########\033[0m"
             os.makedirs(auto_config.software_dir)
-            self.software()
-            self.initialization()
+            self.software_mysql()
+            self.initialization_mysql()
+
+    def software_memcached(self):
+        # downdoads and install Rely  packages
+        # Libevent
+        if os.path.exists(auto_config.libevent_dir) and os.path.exists(auto_config.zlib_dir):
+            print "\033[32m++++++++++Libevent zlib exists+++++++++++\033[0m"
+        else:
+            print "\033[32m#################DownLoad %s ##################" % auto_config.libevent_pack_name
+            command_line1 = "yum -y install wget tar && cd %s && wget %s" % (
+                auto_config.software_dir, auto_config.libevent_url)
+            subprocess.call(command_line1, shell=True)
+            command_line2 = "cd %s && tar xzf %s && cd %s && ./configure %s && make && make install" % (
+                auto_config.software_dir, auto_config.libevent_pack_name, auto_config.libevent_name,
+                auto_config.libevent_parameters)
+            subprocess.call(command_line2, shell=True)
+            # Zlib
+            command_line3 = "cd %s && wget %s" % (auto_config.software_dir, auto_config.zlib_url)
+            command_line4 = "cd %s && tar xzf %s && cd %s && ./configure %s && make && make install" % (
+                auto_config.software_dir, auto_config.zlib_pack_name, auto_config.zlib_name,
+                auto_config.zlib_paramenters)
+            subprocess.call(command_line3, shell=True)
+            subprocess.call(command_line4, shell=True)
+
+    def install_memcached(self):
+        # downloads and install memcacehd
+        if os.path.exists(auto_config.memcached_install_dir):
+            print "\033[32m++++++++++Memcacehd Is Installed++++++++++\033[0m"
+        else:
+            command_line1 = "cd %s && wget %s && tar xzf %s && cd %s && ./configure %s && make && make install" % (
+                auto_config.software_dir, auto_config.memcached_url, auto_config.memcached_pack_name,
+                auto_config.memcached_name, auto_config.memcached_parameters)
+            subprocess.call(command_line1, shell=True)
+            if os.path.exists(auto_config.memcached_install_dir):
+                print "\033[32m++++++++++Install Memcached Success++++++++++\033[0m"
+            else:
+                print "\033[31m++++++++++Install Memcached Failed++++++++++\033[0m"
+
+    def start_memcacehd(self):
+        # start memacehd(11211-11214)
+        command_line2 = "ps aux | grep memcached | grep -v grep | wc -l"
+        p = subprocess.Popen(command_line2, shell=True, stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        if out != 0:
+            print "\033[32m##########The Memcached Is Stared##########\033[0m"
+        else:
+            for i in auto_config.memcached_prot:
+                command_line1 = "%s/bin/memcached -d -m 10 -u root -l 0.0.0.0 -p %s -c 512 -P /tmp/memcached.pid" % (
+                    auto_config.memcached_install_dir, i)
+                print "\033[32mStart Memcached port is %s....................\033[0m" % i
+                subprocess.call(command_line1, shell=True)
+                time.sleep(5)
+
+    def software_mongodb(self):
+        # 安装 mongodb
+        if os.path.exists(auto_config.mongodb_default_dir):
+            print "\033[32m##########Mongodb Is Installed##########\033[0m"
+        else:
+            if os.path.exists(auto_config.software_dir):
+                command_line1 = "yum -y install wget tar && cd %s && wget %s && tar xzf %s && mv %s %s" % (
+                    auto_config.software_dir, auto_config.mongodb_url, auto_config.mongodb_pack_name,
+                    auto_config.mongodb_name,
+                    auto_config.mongodb_default_dir)
+                subprocess.call(command_line1, shell=True)
+                # 添加配置文件
+                if os.path.exists(auto_config.mongodb_data_dir):
+                    configfile_dir = "%s/mongodb.conf" % auto_config.mongodb_default_dir
+                    files = open(configfile_dir, 'w')
+                    for config in auto_config.mongodb_config:
+                        files.write(config)
+                    files.close()
+                else:
+                    os.makedirs(auto_config.mongodb_data_dir)
+                    configfile_dir = "%s/mongodb.conf" % auto_config.mongodb_default_dir
+                    files = open(configfile_dir, 'w')
+                    for config in auto_config.mongodb_config:
+                        files.write(config)
+                    files.close()
+            else:
+                os.makedirs(auto_config.software_dir)
+                command_line1 = "yum -y install wget tar && cd %s && wget %s && tar xzf %s && mv %s %s" % (
+                    auto_config.software_dir, auto_config.mongodb_url, auto_config.mongodb_pack_name,
+                    auto_config.mongodb_name,
+                    auto_config.mongodb_default_dir)
+                subprocess.call(command_line1, shell=True)
+                # 添加配置文件
+                if os.path.exists(auto_config.mongodb_data_dir):
+                    configfile_dir = "%s/mongodb.conf" % auto_config.mongodb_default_dir
+                    files = open(configfile_dir, 'w')
+                    for config in auto_config.mongodb_config:
+                        files.write(config)
+                    files.close()
+                else:
+                    os.makedirs(auto_config.mongodb_data_dir)
+                    configfile_dir = "%s/mongodb.conf" % auto_config.mongodb_default_dir
+                    files = open(configfile_dir, 'w')
+                    for config in auto_config.mongodb_config:
+                        files.write(config)
+                    files.close()
+
+    def start_mongodb(self):
+        # 判断 mongodb 服务是否启动
+        command_line1 = "ps aux | grep mongodb | grep -v 'grep'| wc -l"
+        p = subprocess.Popen(command_line1, shell=True, stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        if out != 0:
+            print "\033[32m##########Mongodb Server Is Started##########\033[0m"
+        else:
+            # 启动 mongodb
+            command_line2 = "%s/mongod -f %s/mongodb.conf" % (
+                auto_config.mongodb_bin_home, auto_config.mongodb_default_dir)
+            subprocess.call(command_line2, shell=True)
 
 
 if __name__ == "__main__":
     start = Install_Mysql()
-    help_man = """
-            此脚本为安装 mysql 实现自动化安装,所有的配置信息存放在 auto_config配置文件中
-            脚本执行过程需要2步认为干预,
-            1,选择 mysql 的版本
-            2,进行 Mysql 的初始密码设定
-                """
-    print help_man
-    time.sleep(20)
-    start.first_reday()
+    server_list = ['mysql', 'memcached', 'mongodb', 'quit']
+    while True:
+        try:
+            for index, value in enumerate(server_list):
+                print index, value
+            chose_server = raw_input("\033[31mPlease Chose Input server:\033[0m")
+            if chose_server.isdigit():
+                chose_server = int(chose_server)
+                if server_list[chose_server] == 'mysql':
+                    start.reday_mysql()
+                    break
+                elif server_list[chose_server] == 'memcached':
+                    start.software_memcached()
+                    start.install_memcached()
+                    start.start_memcacehd()
+                    break
+                elif server_list[chose_server] == 'mongodb':
+                    start.software_mongodb()
+                    start.start_mongodb()
+                    break
+                elif server_list[chose_server] == 'quit':
+                    break
+        except IndexError, err:
+            print "\033[31mChose Error Please reselect\033[0m"
